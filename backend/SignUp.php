11ad2bf111814ae97b116597c6ed96d9731144e6
key00 <kinamugisha@gmail.com>
2023-02-09 14:12:44 +0200

    login/signup app

---
+++ backend/SignUp.php
@@ -0,0 +1,27 @@
+<?php
+include('db.php');
+
+$UserEmail = $decodedData['Email'];
+$UserPW = md5($decodedData['Password']); //password is hashed
+
+$SQL = "SELECT * FROM user WHERE email = '$UserEmail'";
+$exeSQL = mysqli_query($conn, $SQL);
+$checkEmail =  mysqli_num_rows($exeSQL);
+
+if ($checkEmail != 0) {
+    $Message = "Already registered";
+} else {
+
+    $InsertQuerry = "INSERT INTO user (email, upassword) VALUES ('$UserEmail', '$UserPW')";
+
+    $R = mysqli_query($conn, $InsertQuerry);
+
+    if ($R) {
+        $Message = "Complete--!";
+    } else {
+        $Message = "Error";
+    }
+}
+$response[] = array("Message" => $Message);
+
+echo json_encode($response);
